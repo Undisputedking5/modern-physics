4730dd3e299389596478b884a22f036843f9fc94
Undisputedking5 <grayhawi2008@gmail.com>
2026-04-09 13:37:20 +0300

    vercel upgrade

--- modern/settings.py
+++ modern/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w2z$22&1cllx468p=5t4=9gn9cksd1uka%pobp=w6xy4a=)e2p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
